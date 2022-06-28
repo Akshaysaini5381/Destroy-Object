489b66c420b58bda10efbb80d85ca06b22bb0d98
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-06-29 02:14:59 +0530

    Add project files.

---
+++ Destroy Object/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Destroy_Object
+{
+    class C1
+    {
+        public C1()
+        {
+            Console.WriteLine("Constructor :");
+        }
+        static C1()
+        {
+            Console.WriteLine("static constructor :");
+        }
+        public void show()
+        {
+            Console.WriteLine("Method :");
+        }
+        public string name { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            C1 obj = new C1();
+            obj.show();
+         
+            obj.name = "akshay";
+            Console.WriteLine(obj.name);
+            obj = null;
+            
+        }
+    }
+}
